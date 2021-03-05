70186e7f526576f284a52e0f53d1743b4f0c2179
whduddhks <jyy1551@naver.com>
2021-03-06 00:32:59 +0900

    메인 페이지 예시 및 base

--- playlist/settings.py
+++ playlist/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'playlist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['playlist/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
