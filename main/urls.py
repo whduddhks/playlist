76751a477aa2119ca3ac1a35d00a502eab1ff03d
whduddhks <jyy1551@naver.com>
2021-03-06 02:39:49 +0900

    모델 생성

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('detail/<int:detail_id>', views.detail, name="detail"),
+    path('write/', views.write, name="write"),
+    path('create/', views.create, name="create"),
+    path('delete/<int:delete_blog_id>', views.delete, name="delete"),
 ]
