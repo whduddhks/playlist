97cca3c6096df8b045ffdbdbd145a6ea231d743f
whduddhks <jyy1551@naver.com>
2021-03-06 00:56:39 +0900

    static, media, requirement 파일 정리

--- playlist/urls.py
+++ playlist/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('main/', include('main.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
